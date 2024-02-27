d6d80894eebb3884461a88d8020b99c6092d3737
moma14 <151698538+moma14@users.noreply.github.com>
2024-02-26 23:25:12 -0600

    insercion de logo y titulo de la pagina

---
+++ Java/script.js
@@ -0,0 +1,6 @@
+//document.addEventListener("DOMContentLoaded", function() {
+const logo = document.getElementById("logo");
+
+
+logo.src="/CSS/images/f92d5f2f7d56636846861c458c0d0b6c.svg"
+console.log("nyaaaaaaaaaaaa");
