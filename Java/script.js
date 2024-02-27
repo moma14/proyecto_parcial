c2820bfbeb311cb718ed455cc574ee10c651614d
moma14 <151698538+moma14@users.noreply.github.com>
2024-02-27 00:24:40 -0600

    Creacion del arreglo para el menu de idiomas

--- Java/script.js
+++ Java/script.js
@@ -1,6 +1,42 @@
 //document.addEventListener("DOMContentLoaded", function() {
 const logo = document.getElementById("logo");
-
-
 logo.src="/CSS/images/f92d5f2f7d56636846861c458c0d0b6c.svg"
-console.log("nyaaaaaaaaaaaa");
+
+//Creacion de la cajita de menu
+const dropMenu= document.createElement("select");
+const defaultOption=document.createElement("option")
+
+//Creacion de titulo y opciones del menu
+defaultOption.value=""
+defaultOption.text="Idiomas"
+dropMenu.add(defaultOption)
+
+//creacion de un arreglo para automatizar la insercion de los elementos del menu
+const idiomas= [{ nombre: "Español", emoji: "🇪🇸" },
+{ nombre: "Inglés", emoji: "🇬🇧" },
+{ nombre: "Ruso", emoji: "🇷🇺" },
+{ nombre: "Francés", emoji: "🇫🇷" },
+{ nombre: "Alemán", emoji: "🇩🇪" }
+]
+idiomas.forEach(function(idioma){
+    const options=document.createElement("option")
+    options.value=idioma.nombre.toLowerCase()
+    options.text=idioma.emoji+" "+idioma.nombre
+    dropMenu.add(options)
+})
+const menu=document.getElementById("menu")
+menu.appendChild(dropMenu)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
